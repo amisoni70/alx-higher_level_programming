297e84129bfb937b3c15d90b4598f563cec39bb3
amisoni70 <amisoni7014@gmail.com>
2023-06-08 15:33:43 -0700

    revised

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,13 +3,7 @@ import sys
 
 if __name__ == "__main__":
 
-    count = len(sys.argv) - 1
-
-    p = 0
-    total = 0
-    for arg in sys.argv:
-        if p != 0:
-            total += int(arg)
-        else:
-            p += 1
-        print("{:d}".format(total))
+    sum = 0
+    for p in range(1, len(sys.argv)):
+        sum = sum + int(sys.argv[p])
+    print(sum)
