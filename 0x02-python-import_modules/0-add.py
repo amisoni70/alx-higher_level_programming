3a1f0d5ca8db4073a1d91863931b7bb59966661e
amisoni70 <amisoni7014@gmail.com>
2023-06-08 05:16:13 -0700

    Task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    ""Prints the addition of 1 and 2""
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+These are my tasks on Python import and modules
