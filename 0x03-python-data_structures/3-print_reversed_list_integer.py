cc5e6b2de8e90f782c4cf055e857bbb229318c7c
amisoni70 <amisoni7014@gmail.com>
2023-06-12 07:04:46 -0700

    Function that prints integers in an array in reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for k in my_list:
-        print("{:d}".format(k))
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for k in my_list:
+            print("{:d}".format(k))
