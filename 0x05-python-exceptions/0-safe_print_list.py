ae8e09d01b0b841ebe3f46b33b5947c8c3c6c0ea
amisoni70 <amisoni7014@gmail.com>
2023-06-26 02:50:17 -0700

    function that prints x elements in a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+    sum_of_elements = 0
+    for p in range(x):
+        try:
+            print("{:d}".format(my_list[p]), end="")
+            sum_of_elements += 1
+        except IndexError:
+            break
+    print("")
+    return(sum_of_elements)
