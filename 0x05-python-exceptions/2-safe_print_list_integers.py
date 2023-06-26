fd1d385a434ae480cae694f5087ef73851170f9f
amisoni70 <amisoni7014@gmail.com>
2023-06-26 04:28:51 -0700

    Taskk 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[p]), end="")
             sum_of_elements += 1
-        except(ValueError, TypeError):
+        except(ValueError,TypeError):
             continue
     print("")
     return (sum_of_elements)
