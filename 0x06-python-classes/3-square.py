ce81f4e639dc1f1b44a1cfbf30a764f6bc2ea0cf
amisoni70 <amisoni7014@gmail.com>
2023-06-27 10:02:41 -0700

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Definition of a class square"""
+
+
+class Square:
+
+    """class that defines a square by:(based on 2-square.py)"""
+
+    def __init__(self, size=0):
+
+        """creates new instances of the square
+
+        Args:
+        The size of the square"""
+
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculates the area of the square"""
+
+        """Returns its area"""
+
+        return self.__size ** 2
