4c047d58a8d46c5fd8a7cbd97aafce7a49c24e62
amisoni70 <amisoni7014@gmail.com>
2023-06-27 07:48:29 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Definition of a class square"""
+
+
+class Square:
+
+    """class that defines a square by:(based on 0-square.py)"""
+
+    def __init__(self, size):
+
+        """creates new instances of the square
+
+        Args:
+        The size of the square"""
+
+        self.__size = size
