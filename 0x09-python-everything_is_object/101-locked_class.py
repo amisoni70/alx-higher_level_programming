0d173d1a9ad86fdb843ca2f6999c9f2b66c6dc86
amisoni70 <amisoni7014@gmail.com>
2023-07-06 11:41:55 -0700

    Task 101

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class LockedClass:
+
+    """prevents the  prevents the user from dynamically creating new instance
+    attributes except if the new instance attribute is first_name"""
+
+    """Attributes:
+    first_name : first name of anything"""
+
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+
+        """Creates an instance of LockedClass"""
+
+        self.first_name = "first_name"
