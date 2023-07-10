9751c1e7f4ff4958b5d865b5bac2f4c802e28d23
amisoni70 <amisoni7014@gmail.com>
2023-07-10 13:36:53 -0700

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Definition of a square"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """class square"""
+    def __init__(self, size):
+        """initialisation"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the area"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Return a string"""
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
