8f8e86a5862e3ab13e724c879458f0392c12f15e
amisoni70 <amisoni7014@gmail.com>
2023-07-11 07:07:19 -0700

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/sur/bin/python3
+
+"""Function that appends a string at the end of a text file"""
+
+
+def append_write(filename="", text=""):
+    """appends a string at the end of .txt file
+    Return: no of characters added"""
+    with open(filename, 'a') as f:
+        characters = f.write(text)
+        return characters
