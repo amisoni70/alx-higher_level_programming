6904898613e3c5d4f94429eebaeaafe6acfe9480
amisoni70 <amisoni7014@gmail.com>
2023-07-16 05:03:32 -0700

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Definition of a class named Base. This class will be the “base” of all other
+classes in this project."""
+
+
+class Base:
+    """Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialisation
+
+        Args:
+        id (int): object id. Defaults to none.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
