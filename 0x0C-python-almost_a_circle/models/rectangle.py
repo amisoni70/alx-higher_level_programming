f963c2ee4729a0ffe89a19494711438aed41469b
amisoni70 <amisoni7014@gmail.com>
2023-07-16 10:23:54 -0700

    Task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -123,3 +123,10 @@ class Rectangle(Base):
             for k in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """prints the rectangle"""
+
+        return ("[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}".
+                format(self.id, self.__x, self.__y, self.__width,
+                       self.__height))
