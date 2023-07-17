ac48fe7974d51a5eedb08164c3df6cfca3267f1d
amisoni70 <amisoni7014@gmail.com>
2023-07-17 06:55:21 -0700

    Task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON representation of a list of
+        dictionaries
+
+        Args: list (list of dictionaries)
+
+        Return: JSON String (Str)
+        """
+
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
+        return json.dumps(list_dictionaries)
