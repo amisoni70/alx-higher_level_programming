723df5b5a5ee1800887975b1ba0674bc68c79ec4
amisoni70 <amisoni7014@gmail.com>
2023-07-17 02:38:50 -0700

    Task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,43 @@ class Square(Rectangle):
 
         return ("[Square] ({}) {:d}/{:d} - {:d}".
                 format(self.id, self.x, self.y, self.width))
+
+    def update(self, *args, **kwargs):
+        """Updates the Square.
+
+        Args:
+        1st Arguement: id attribute
+        2nd Arguement: size attribute
+        3rd Arguement: x attribute
+        4th Arguement: y attribute
+        """
+
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a = a + 1
+
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if key is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
