c9a2abffe940b5021a83f925def1459bab8f518b
amisoni70 <amisoni7014@gmail.com>
2023-08-16 07:20:36 -0700

    Task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- Creating a subquery
-SELECT id name;
-FROM cities;
-WHERE state_id = (SELECT id FROM states WHERE name="California");
-ORDER BY id ASC
+SELECT id, name
+FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name="California")
+ORDER BY id ASC;
