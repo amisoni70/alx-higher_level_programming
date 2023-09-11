5694451148bcc68dbe9fc48d8494dce837b8d88f
amisoni70 <amisoni7014@gmail.com>
2023-09-11 06:17:07 -0700

    Task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Prints the message according to the no. of arguements
+
+if (process.argv.length === 2){
+	console.log('No argument');
+} else if (process.argv.length === 3){
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
