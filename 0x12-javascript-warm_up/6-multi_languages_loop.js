d0aa4b20e3158508edfa3cfea398056735bc8225
amisoni70 <amisoni7014@gmail.com>
2023-09-11 09:43:44 -0700

    Task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script that prints 3 lines using a for loop
+
+
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let k = 0; k < 3; k++) {
+  console.log(args[k]);
+}
