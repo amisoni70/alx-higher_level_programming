3ad953def7af60f53f46109855e67dc6aa9952d0
amisoni70 <amisoni7014@gmail.com>
2023-09-11 09:19:39 -0700

    Task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that prints an integer 
+
+if (isNaN(process.argv[2])) {
+  console.log('Not a Number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
