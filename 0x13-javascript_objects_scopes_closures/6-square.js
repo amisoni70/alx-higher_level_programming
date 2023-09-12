d17730a5d422b9486230ef1d45d3316596ff2c34
amisoni70 <amisoni7014@gmail.com>
2023-09-12 10:55:01 -0700

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// definition of a square class that inherits from rectangle
+
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
