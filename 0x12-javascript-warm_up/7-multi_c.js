7b3dd335735fc4aac486e06bffcdaa2f21f96d9b
amisoni70 <amisoni7014@gmail.com>
2023-09-13 09:02:55 -0700

    Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,6 @@ const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let k = 0; k < x; k++); {
+  for (let k = 0; k < x; k++);
   console.log('C is fun');
-  }
 }
