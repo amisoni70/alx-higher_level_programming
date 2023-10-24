705754d306a49d642e5027a66379b742d408f651
amisoni70 <amisoni7014@gmail.com>
2023-10-24 09:17:33 -0700

    Task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// A script that gets the contents of a webpage and stores it in a file
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const path = process.argv[3];
+
+request(url).pipe(fs.createWriteStream(path));
