b28e3383b872dfb1508df8ce94ccc2960bac2e34
amisoni70 <amisoni7014@gmail.com>
2023-10-24 06:58:59 -0700

    Task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,8 +4,10 @@
 const url = process.argv[2];
 const request = require('request');
 
-request(url, function (error, body, response) {
+request(url, function (error, response, body) {
   if (error) {
     console.log('error:', error);
-  } else console.log('code:', response && response.statusCode);
+  } else {
+    console.log('code:', response && response.statusCode);
+  }
 });
